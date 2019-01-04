105698d88d0f3abb02cb80bd4bbfb0c329317a45
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-01-04 19:29:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const express=require('express');
-//创建服务器
+const express=require("express");
+const bodyParser=require("body-parser");
+const userRouter=require("./router/user");
+const productsRouter=require("./router/products");
+
+//创建web服务器
 var server=express();
-server.listen(8080);
-server.use(express.static('public'));
+server.listen(3001);
+//托管静态资源
+server.use(express.static("public"));
+
+// /user/register
+server.use("/user",userRouter);
+//把商品路由器挂载到/product
+// /product/test
+server.use("/products",productsRouter);
+
+
