3bfaf69f1c07769818b76e3d05d311e1d55afdbc
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-01-27 15:54:38 +0800

    Add files via upload

--- router/products.js
+++ router/products.js
@@ -1,6 +1,10 @@
 const express=require("express");
+//引入连接池
+const pool=require("../pool.js");
+//路由器
 var router=express.Router();
-router.get('',(req,res)=>{
+router.get("",(req,res)=>{
     
 
 });
+module.exports=router;
--- router/user.js
+++ router/user.js
@@ -4,15 +4,34 @@ const express=require("express");
 var router=express.Router();
 router.post("/register",(req,res)=>{
     var obj=req.body;
+    //console.log(obj);
     var $sname=obj.sname;
     if(!$sname){
         res.send({code:401,msg:"sname required"});
         return;
     }
-    var $upwd=obj.upwd;
-    if(!$upwd){
+    var $supwd=obj.supwd;
+    if(!$supwd){
         res.send({code:402,msg:"supwd required"});
+        return;
     }
+    var $semail=obj.semail;
+    if(!$semail){
+        res.send({code:402,msg:"semail required"});
+        return;
+    }
+    var $sphone=obj.sphone;
+    if(!$sphone){
+      res.send({code:403,msg:"phone required"});
+      return;
+    }
+   pool.query("INSERT INTO star_register SET ?",[obj],(err,result)=>{
+       if(err) throw err;
+       if(result.affectedRows>0){
+           console.log(55);
+           res.send({code:200,msg:"register success"});
+       }
+   });
 });
 //导出路由
 module.exports=router;
