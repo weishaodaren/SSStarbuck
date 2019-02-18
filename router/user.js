0d536b43e0c34786fa79c9e0a1e9c43c1ef9947b
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-02-18 08:42:02 +0800

    Add files via upload
    new js

--- router/user.js
+++ router/user.js
@@ -33,5 +33,21 @@ router.post("/register",(req,res)=>{
        }
    });
 });
+
+//创建用户登录路由
+router.get('/login',(req,res)=>{
+    var sname=req.query.sname;
+    var spwd=req.query.spwd;
+    var sql='SELECT sid FROM star_login WHERE suname= ? AND supwd= md5(?)';
+    pool.query(sql,[sname,spwd],(err,result)=>{
+        if(err) throw err;
+        if(result.length==0){
+            res.send({code:0,msg:`用户名或密码有误`});
+        }else{
+            res.send({code:1,msg:`登录成功`});
+        }
+    });
+});
+
 //导出路由
 module.exports=router;
