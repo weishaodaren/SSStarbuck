2b33e3322f564e9f86ea0effa22af8423db5b511
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-02-21 09:16:26 +0800

    Add files via upload
    add login and register use ajax

--- router/user.js
+++ router/user.js
@@ -2,6 +2,7 @@ const pool=require("../pool.js");
 const express=require("express");
 //创建路由器
 var router=express.Router();
+//注册路由
 router.post("/register",(req,res)=>{
     var obj=req.body;
     //console.log(obj);
@@ -25,10 +26,9 @@ router.post("/register",(req,res)=>{
       res.send({code:403,msg:"phone required"});
       return;
     }
-   pool.query("INSERT INTO star_register SET ?",[obj],(err,result)=>{
+   pool.query("INSERT INTO star_users SET ?",[obj],(err,result)=>{
        if(err) throw err;
        if(result.affectedRows>0){
-           console.log(55);
            res.send({code:200,msg:"register success"});
        }
    });
@@ -37,9 +37,9 @@ router.post("/register",(req,res)=>{
 //创建用户登录路由
 router.get('/login',(req,res)=>{
     var sname=req.query.sname;
-    var spwd=req.query.spwd;
-    var sql='SELECT sid FROM star_login WHERE suname= ? AND supwd= md5(?)';
-    pool.query(sql,[sname,spwd],(err,result)=>{
+    // var supwd=req.query.spwd;
+    var sql='SELECT sid FROM star_users WHERE sname =?';
+    pool.query(sql,[sname],(err,result)=>{
         if(err) throw err;
         if(result.length==0){
             res.send({code:0,msg:`用户名或密码有误`});
