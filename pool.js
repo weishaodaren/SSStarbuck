e05a4622f4fc6147c94733f9e1827747690ff40a
weishaodaren <king.zjw@qq.com>
2019-04-04 16:48:32 +0800

    维护 dubug

--- pool.js
+++ pool.js
@@ -1,10 +1,16 @@
 const mysql=require("mysql");
 var pool=mysql.createPool({
-    host     : process.env.MYSQL_HOST,
-    port     : process.env.MYSQL_PORT,
-    user     : process.env.ACCESSKEY,
-    password : process.env.SECRETKEY,
-    database : 'app_' + process.env.APPNAME,
-    connectionLimit: 3
+    // host     : process.env.MYSQL_HOST,
+    // port     : process.env.MYSQL_PORT,
+    // user     : process.env.ACCESSKEY,
+    // password : process.env.SECRETKEY,
+    // database : 'app_' + process.env.APPNAME,
+    // connectionLimit: 3
+    host:'127.0.0.1',
+    port:'3306',
+    user:'root',
+    password:'',
+    database:'starbuck',
+    connectionLimit:10
 });
 module.exports=pool;
